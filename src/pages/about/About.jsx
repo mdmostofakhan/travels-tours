183a6b411f7011a52ddc5cf2056458381901e7d3
mostofa khan <mdmostofakhan1972@gmail.com>
2023-09-23 13:32:46 +0600

    seceond

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Icon } from '@iconify/react';
-
+import image1 from '../../assets/image1.jpg'
+import image2 from '../../assets/image2.jpg'
 const About = () => {
   return (
     <div className="container mx-auto">
-      <div className="mt-6">
+      <div className="grid grid-cols-2 gap-8 mt-12">
         <div className="w-auto">
           <h2 className="text-[#c29d59] text-xl">Best agency</h2>
           <p className="text-black font-semibold text-6xl mt-2">Why choose our <span className="text-[#c29d59] font-semibold text-6xl">travel</span></p>
@@ -14,20 +15,32 @@ const About = () => {
                 <p className="mt-2">exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 <p className="mt-6">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat</p>
                 <p className="mt-2">nulla pariatur. Excepteur sint occaecat cupidatat non proident, s</p>
-            <div className="mt-8">
-                  <div className="bg-[#c29d59] border text-center justify-center  text-white w-[58px] h-[58px] rounded-full">
+            <div className=" grid grid-cols-2 mt-8">
+                <div>
+                <div className="bg-[#c29d59] border text-center justify-center  text-white w-[58px] h-[58px] rounded-full">
                   <Icon className=" text-4xl font-semibold mt-2 ml-2"  icon="arcticons:supercasts" />
                   </div>
                    <p className="text-black font-semibold text-2xl mt-2">Trust & Safety</p>
-                   <p >All our cars come from main dealers and</p>
-                   <p>go through rigorous checks and tests</p>
-                   <p>before they arrive at your door</p>
-            </div>
-        </div>
-
-
-        <div>
-          <img src="" alt="" />
+                   <p className="mt-6" >All our cars come from main dealers and</p>
+                   <p>go through rigorous checks and tests before they <br />arrive at your door</p>
+                   
+                </div>
+                <div>
+                <div className="bg-[#c29d59] border text-center justify-center  text-white w-[58px] h-[58px] rounded-full">
+                  <Icon className=" text-4xl font-semibold mt-2 ml-2"  icon="arcticons:supercasts" />
+                  </div>
+                   <p className="text-black font-semibold text-2xl mt-2">Trust & Safety</p>
+                   <p className="mt-6" >All our cars come from main dealers and</p>
+                   <p>go through rigorous checks and tests before they <br /> arrive at your door</p>
+                   
+                </div>
+                </div>
+                <p className="mt-6">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint <br /> occaecat cupidatat non proident, s</p>
+                  <p className="mt-10">Make your holiday Special</p>
+           </div>
+        <div className="relative">
+          <img className=" w-9/12 absolute right-6 h-full" src={image1} alt="" />
+          <img className=" w-9/12  absolute 0-left-8 top-[420px]" src={image2} alt="" />
         </div>
       </div>
     </div>
