9dae994b6ea9735634050b22815a630b0abe09a8
mostofa khan <mdmostofakhan1972@gmail.com>
2023-09-28 00:49:57 +0600

    bg

--- src/pages/about/MainAbout.jsx
+++ src/pages/about/MainAbout.jsx
@@ -11,8 +11,6 @@ import "swiper/css/navigation";
 // import required modules
 import { FreeMode, Pagination } from "swiper/modules";
 
-
-
 const MainAbout = () => {
   return (
     <div>
@@ -30,98 +28,220 @@ const MainAbout = () => {
       </div>
       <div className=" mt-10 flex justify-center  relative">
         <div className=" font-bold text-[200px]">
-        <h1 className="text-[#f9f7f3]  ">TRENDING</h1>
+          <h1 className="text-[#f9f7f3]  ">TRENDING</h1>
         </div>
         <div className=" top-24 absolute">
-        <h1 className="text-center text-4xl italic font-bold">Destinations</h1>
-            <p className="text-center">Lorem ipsum dolor sit amet, conseetuer adipiscing elit. Aenan comdo <br />
-                igula eget. Aenean massa cum sociis Theme natoque</p>
+          <h1 className="text-center text-4xl italic font-bold">
+            Destinations
+          </h1>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet, conseetuer adipiscing elit. Aenan comdo{" "}
+            <br />
+            igula eget. Aenean massa cum sociis Theme natoque
+          </p>
         </div>
       </div>
 
-      <div>
-       <>
-        <Swiper
-          slidesPerView={3}
-          spaceBetween={30}
-          freeMode={true}
-          // pagination={{
-          //   clickable: true,
-          // }}
-          modules={[FreeMode, Pagination]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <div className="h-[350px] ">
-              <img
-                src="https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="h-[350px] ">
-              <img
-                src="https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="h-[350px] ">
-              <img
-                src="https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="h-[350px] ">
-              <img
-                src="https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="h-[350px] ">
-              <img
-                src="https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="h-[350px] ">
-              <img
-                src="https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="h-[350px] ">
-              <img
-                src="https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-        </Swiper>
-      </>
-       </div>
+      <div className="w-full">
+        <>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={30}
+            freeMode={true}
+            // pagination={{
+            //   clickable: true,
+            // }}
+            modules={[FreeMode, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="h-[400px] relative">
+                <div className="">
+                  <h1 className="absolute top-32 text-5xl text-white italic right-48">
+                    Vietnam{" "}
+                  </h1>
+                  <div className="bg-[#c29d59]  absolute text-center end-8 bottom-1 text-white w-[64px] h-[64px] rounded-full">
+                    <h1 className=" text-xl text-center">15 Tours</h1>
+                  </div>
+                </div>
+                <img
+                  src="https://gico.io/ghurbo/demos/ghurbo/images/resource/destination-2.jpg"
+                  className="bg-cover"
+                  alt=""
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="h-[400px] relative">
+                <div className="">
+                  <h1 className="absolute top-32 text-5xl text-white italic right-48">
+                    Indonesia
+                  </h1>
+                  <div className="bg-[#c29d59]  absolute text-center end-8 bottom-1 text-white w-[64px] h-[64px] rounded-full">
+                    <h1 className=" text-xl text-center">40 Tours</h1>
+                  </div>
+                </div>
+                <img
+                  src="https://gico.io/ghurbo/demos/ghurbo/images/resource/destination-1.jpg"
+                  className="bg-cover"
+                  alt=""
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="h-[400px] relative">
+                <div className="">
+                  <h1 className="absolute top-32 text-5xl text-white italic right-48">
+                    Pakistan{" "}
+                  </h1>
+                  <div className="bg-[#c29d59]  absolute text-center end-8 bottom-1 text-white w-[64px] h-[64px] rounded-full">
+                    <h1 className=" text-xl text-center">21 Tours</h1>
+                  </div>
+                </div>
+                <img
+                  src="https://gico.io/ghurbo/demos/ghurbo/images/resource/destination-4.jpg"
+                  className="bg-cover"
+                  alt=""
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="h-[400px] relative">
+                <div className="">
+                  <h1 className="absolute top-32 text-5xl text-white italic right-48">
+                    Thailend{" "}
+                  </h1>
+                  <div className="bg-[#c29d59]  absolute text-center end-8 bottom-1 text-white w-[64px] h-[64px] rounded-full">
+                    <h1 className=" text-xl text-center">60 Tours</h1>
+                  </div>
+                </div>
+                <img
+                  src="https://gico.io/ghurbo/demos/ghurbo/images/resource/destination-3.jpg"
+                  className="bg-cover"
+                  alt=""
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="h-[400px] relative">
+                <div className="">
+                  <h1 className="absolute top-32 text-5xl text-white italic right-48">
+                    vietnam{" "}
+                  </h1>
+                  <div className="bg-[#c29d59]  absolute text-center end-8 bottom-1 text-white w-[64px] h-[64px] rounded-full">
+                    <h1 className=" text-xl text-center">15 Tours</h1>
+                  </div>
+                </div>
+                <img
+                  src="https://gico.io/ghurbo/demos/ghurbo/images/resource/destination-2.jpg"
+                  className="bg-cover"
+                  alt=""
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="h-[400px] relative">
+                <div className="">
+                  <h1 className="absolute top-32 text-5xl text-white italic right-48">
+                    Indonesia
+                  </h1>
+                  <div className="bg-[#c29d59]  absolute text-center end-8 bottom-1 text-white w-[64px] h-[64px] rounded-full">
+                    <h1 className=" text-xl text-center">40 Tours</h1>
+                  </div>
+                </div>
+                <img
+                  src="https://gico.io/ghurbo/demos/ghurbo/images/resource/destination-1.jpg"
+                  className="bg-cover"
+                  alt=""
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="h-[400px] relative">
+                <div className="">
+                  <h1 className="absolute top-32 text-5xl text-white italic right-48">
+                    Pakistan
+                  </h1>
+                  <div className="bg-[#c29d59]  absolute text-center end-8 bottom-1 text-white w-[64px] h-[64px] rounded-full">
+                    <h1 className=" text-xl text-center">21 Tours</h1>
+                  </div>
+                </div>
+                <img
+                  src="https://gico.io/ghurbo/demos/ghurbo/images/resource/destination-4.jpg"
+                  className="bg-cover"
+                  alt=""
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="h-[400px] relative">
+                <div className="">
+                  <h1 className="absolute top-32 text-5xl text-white italic right-48">
+                    Thailand{" "}
+                  </h1>
+                  <div className="bg-[#c29d59]  absolute text-center end-8 bottom-1 text-white w-[64px] h-[64px] rounded-full">
+                    <h1 className=" text-xl text-center">60 Tours</h1>
+                  </div>
+                </div>
+                <img
+                  src="https://gico.io/ghurbo/demos/ghurbo/images/resource/destination-3.jpg"
+                  className="bg-cover"
+                  alt=""
+                />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </>
+      </div>
 
-       <div className=" mt-10 flex justify-center  relative">
+      <div className=" mt-10 flex justify-center  relative">
         <div className=" font-bold text-[200px]">
-        <h1 className="text-[#f9f7f3] ">Teams</h1>
+          <h1 className="text-[#f9f7f3] ">Teams</h1>
         </div>
         <div className=" top-24 absolute">
-        <h1 className="text-center text-4xl italic font-bold">Our Teams</h1>
-            <p className="text-center">Lorem ipsum dolor sit amet, conseetuer adipiscing elit. Aenan comdo <br />
-                igula eget. Aenean massa cum sociis Theme natoque</p>
+          <h1 className="text-center text-4xl italic font-bold">Our Teams</h1>
+          <p className="text-center">
+            Lorem ipsum dolor sit amet, conseetuer adipiscing elit. Aenan comdo{" "}
+            <br />
+            igula eget. Aenean massa cum sociis Theme natoque
+          </p>
         </div>
       </div>
-      
+
+      <div className="flex justify-center gap-5 mt-8">
+        <div className="relative">
+               <div className="absolute hover:bg-red-400">
+               <p>Tania</p>
+                <p>Cto Travel</p>
+               </div>
+          <img
+            src="https://gico.io/ghurbo/demos/ghurbo/images/resource/team-1.jpg"
+            className="w-[300px]"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://gico.io/ghurbo/demos/ghurbo/images/resource/team-2.jpg"
+            className="w-[300px]"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://gico.io/ghurbo/demos/ghurbo/images/resource/team-3.jpg"
+            className="w-[300px]"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://gico.io/ghurbo/demos/ghurbo/images/resource/team-4.jpg"
+            className="w-[300px]"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
